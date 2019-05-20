45fb42bace1a1ea48ad9668adbc7d10a845ba793
MW-Resurgent <mwu2021@gmail.com>
2019-05-19 23:35:26 -0700

    Additions to insight-mvp-b
    Made changes to Dash.jsx and Events.jsx in insight-mvp-b.  Added mvp-components and mvp-variables folder to hold respective information for mock up in insight-mvp-b.

--- insight-mvp-b/src/views/mvp_views/Dash.jsx
+++ insight-mvp-b/src/views/mvp_views/Dash.jsx
@@ -5,8 +5,30 @@ import classNames from "classnames";
 
 // reactstrap components
 import {
-
+  Button,
+  ButtonGroup,
+  Card,
+  CardImg,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  Label,
+  FormGroup,
+  Input,
+  Progress,
+  Table,
+  Row,
+  Col,
+  UncontrolledTooltip
 } from "reactstrap";
+
+// core components
+import EventDetails from "../mvp_views/EventDetails.jsx"
 
 
 class Dash extends React.Component {
@@ -15,7 +37,53 @@ class Dash extends React.Component {
   }
 
   render() {
-    return(<div></div>);
+    return(
+      <div className="content">
+        <Row>
+          <Col sm="4">
+            <Card>
+              <CardHeader>
+                <CardTitle tag="h1">Following</CardTitle>
+              </CardHeader>
+              <CardBody>
+                <EventDetails>
+                </EventDetails>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col sm="4">
+            <Card>
+              <CardHeader>
+                <CardTitle tag="h1">From Your School Department</CardTitle>
+              </CardHeader>
+              <CardBody>
+
+              </CardBody>
+            </Card>
+          </Col>
+          <Col sm="4">
+            <Card>
+              <CardHeader>
+                <CardTitle tag="h1">New on Insight</CardTitle>
+              </CardHeader>
+              <CardBody>
+
+              </CardBody>
+            </Card>
+
+            <Card>
+            <CardHeader>
+              <CardTitle tag="h1">Your Recent Activity</CardTitle>
+              </CardHeader>
+              <CardBody>
+
+              </CardBody>
+            </Card>
+          </Col>
+
+        </Row>
+      </div>
+    );
   }
 }
 
--- insight-mvp-b/src/views/mvp_views/Events.jsx
+++ insight-mvp-b/src/views/mvp_views/Events.jsx
@@ -5,6 +5,13 @@ import classNames from "classnames";
 
 // reactstrap components
 import {
+  Button,
+  ButtonGroup,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
   Row,
   Col,
   Card,
@@ -23,8 +30,64 @@ class EventDetails extends React.Component {
         <div className="content">
           <Row>
             <Col sm="12">
-              <Card className="tags">
-                <CardImg top height="150px" src={require("assets/img/card-danger.png")} alt="..."/>
+              <Card className="event-options">
+                <ButtonGroup>
+                  <Button color="info" type="button">
+                    Group
+                  </Button>
+                  <Button color="info" type="button">
+                    Group Type
+                  </Button>
+                  <Button color="info" type="button">
+                    School Department
+                  </Button>
+                  <Button color="info" type="button">
+                    Interest
+                  </Button>
+                  <Button color="info" type="button">
+                    Event Type
+                  </Button>
+                  <Button color="info" type="button">
+
+                  </Button>
+                </ButtonGroup>
+                <UncontrolledDropdown>
+                  <DropdownToggle
+                    aria-expanded={false}
+                    aria-haspopup={true}
+                    caret
+                    className="btn-block"
+                    color="primary"
+                    data-toggle="dropdown"
+                    id="dropdownMenuButton"
+                    type="button"
+                  >
+                    View As
+                  </DropdownToggle>
+                  <DropdownMenu aria-labelledby="dropdownMenuButton">
+                    <DropdownItem header>
+                      List
+                    </DropdownItem>
+                    <DropdownItem
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      Cards
+                    </DropdownItem>
+                    <DropdownItem
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      Calendar
+                    </DropdownItem>
+                    <DropdownItem
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                    >
+                      Something else here
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
               </Card>
             </Col>
           </Row>
