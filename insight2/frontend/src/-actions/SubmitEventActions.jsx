c604b0c0bc8d55b7258a9a07df684373689463aa
MW-Intrepid <mwu2021@gmail.com>
2019-10-21 21:38:43 -0700

    Added front-end files from insight-mvp-b
    Going to translate work done in insight-mvp-b on the front end into the MaterialUI template used on this frontend.

---
+++ insight2/frontend/src/-actions/SubmitEventActions.jsx
@@ -0,0 +1,51 @@
+import alt from '../alt';
+import axios from 'axios';
+
+class SubmitEventActions {
+    constructor() {
+        this.generateActions(
+            'addEventSuccess',
+            'addEventFail',
+            'updateName',
+            'updateSubtitle',
+            'updateDescription',
+            'updateRSVPCount',
+            'updateStartDate',
+            'updateEndDate',
+            'updateOrg',
+            'updateLocName',
+            'updateStreet',
+            'updateCity',
+            'updateState',
+            'updateCountry',
+            'updateZipcode',
+        );
+    }
+
+    addEvent(name, subtitle, description, rsvpCount, startDate, endDate, org,
+        locName, street, city, state, country, zipcode) {
+        axios.put('/api/event', {
+            name: name,
+            subtitle: subtitle,
+            description: description,
+            rsvpCount: rsvpCount,
+            startDate: startDate,
+            endDate: endDate,
+            org: org,
+            locName: locName,
+            street: street,
+            city: city,
+            state: state,
+            country: country,
+            zipcode: zipcode
+        })
+        .then(function(response) {
+            console.log("Success");
+        })
+        .catch(function(error) {
+            console.log("Failed");
+        });
+    }
+}
+
+export default alt.createActions(SubmitEventActions);
---
+++ insight2/frontend/src/views/-insight/Org.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+
+
+// reactstrap components
+import {
+
+} from "reactstrap";
+
+
+class Org extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(<div></div>);
+  }
+}
+
+export default Org;
